8c4728440ec1792902cec3c651e7d7de67d2ec48
Ayush Kashyap <ayushkashyap0507@gmail.com>
2024-06-09 00:40:02 +0530

    Updated scoring terms

--- src/functions/getNumbers.js
+++ src/functions/getNumbers.js
@@ -5,18 +5,19 @@ const router = Router()
 const GetNumbers=async(req,res)=>{
     var data=req.body
     var amount=data.amount
+    var points=data.points-amount
     var rand=Math.ceil(Math.random()*6)
     var rand2=Math.ceil(Math.random()*6)
     if(data.option==="lucky" && rand+rand2===7){
-        amount=amount*4
+        amount=amount*5
     }
     else if((data.option==="above" && rand+rand2>=7)||(data.option==="below" && rand+rand2<=7)){
-        amount=amount*1
+        amount=amount*2
     }
     else{
-        amount=amount*(-1)
+        amount=0
     }
-    var Totalamount=amount+data.points
+    var Totalamount=amount+points
     console.log(Totalamount)
     await UserModel.updateOne({username:data.username},{$set:{points:Totalamount}})
     res.status(200).send({gained:amount,amount:Totalamount,numbers:{first:rand,second:rand2}})
